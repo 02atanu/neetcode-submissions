3f6b10f9ca59fbcd35029e763253766c16206ceb
Atanu Das <02.atanudas@gmail.com>
2026-08-20 13:59:08 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # Return false if the lengths are different
+        if len(s) != len(t):
+            return False
+        
+        counts = {}
+
+        # Create a Hashmap for storing counts of s
+        for i in s:
+            counts[i] = counts.get(i, 0) + 1
+
+        # Check the hashmap for counts of t
+        for j in t:
+            if j in counts:
+                counts[j] -= 1
+            else:
+                return False
+            if counts[j] < 0:
+                return False
+        return True
