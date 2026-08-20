9829c222762f4d6920fa7ee3d6a1a2c15bf0edcf
Atanu Das <02.atanudas@gmail.com>
2026-08-20 13:49:11 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # Return false if the lengths are different
+        if len(s) != len(t):
+            return False
+        
+        for letter in s:
+            if letter in t:
+                t = t.replace(letter, '', 1)
+            else:
+                return False
+        return True
